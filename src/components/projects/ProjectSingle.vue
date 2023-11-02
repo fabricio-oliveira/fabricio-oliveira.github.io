0eb5e66ecab2a286664e0ed9fe8174024282cab7
Fabricio Oliveira <fabricioque@gmail.com>
2023-11-02 15:03:54 -0300

    adjust at design

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -5,28 +5,19 @@ export default {
 </script>
 
 <template>
-	<router-link
-		:to="`/projects/single-project/${project.id}`"
-		class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
-		aria-label="Single Project"
-	>
-		<div>
-			<img
-				:src="project.img"
-				:alt="project.header.title"
-				class="rounded-t-xl border-none"
-			/>
+	<router-link :to="`/projects/single-project/${project.id}`"
+		class="rounded-xl shadow-lg hover:shadow-xl flex flex-col cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark p-2"
+		aria-label="Single Project">
+		<div class=" flex justify-center h-4/5 p-low-2">
+			<img :src="project.img" :alt="project.header.title" class="rounded-t-xl border-none" />
 		</div>
-		<div class="text-center px-4 py-6">
-			<p
-				class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2"
-			>
+		<div class="h-1/5 p-low-2 pt-2">
+			<p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
 				{{ project.header.title }}
 			</p>
-			<span
-				class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light"
-				>{{ project.category }}</span
-			>
+			<span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light mb-2">
+				{{ project.category }}
+			</span>
 		</div>
 	</router-link>
 </template>
