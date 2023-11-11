01872bf645fd28b048a14466fecc5c3220197075
Fabricio Oliveira <fabricioque@gmail.com>
2023-11-11 13:16:54 -0300

    improvements

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -6,12 +6,12 @@ export default {
 
 <template>
 	<router-link :to="`/projects/single-project/${project.id}`"
-		class="rounded-xl shadow-lg hover:shadow-xl flex flex-col cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark p-2"
+		class="rounded-xl shadow-lg hover:shadow-xl flex flex-col cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark sm:p-8 p-16"
 		aria-label="Single Project">
-		<div class=" flex justify-center h-4/5 p-low-2">
+		<div class=" flex justify-center h-4/5">
 			<img :src="project.img" :alt="project.header.title" class="rounded-t-xl border-none" />
 		</div>
-		<div class="p-low-2 pt-2">
+		<div class="p-low-2 pt-2 ">
 			<p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
 				{{ project.header.title }}
 			</p>
