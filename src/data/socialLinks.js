8a179aeb39d8d4c4f3721a2550f885b8a796f76b
Fabricio Oliveira <fabricioque@gmail.com>
2023-11-11 23:48:51 -0300

    improve social links

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,18 +1,12 @@
 export const socialLinks = [
 	{
 		id: 1,
-		name: 'Website',
-		icon: 'globe',
-		url: 'https://fabricio-oliveira.github.io',
-	},
-	{
-		id: 2,
 		name: 'GitHub',
 		icon: 'github',
 		url: 'https://github.com/fabricio-oliveira',
 	},
 	{
-		id: 3,
+		id: 2,
 		name: 'LinkedIn',
 		icon: 'linkedin',
 		url: 'https://www.linkedin.com/in/fabricio-oliveira-b2b88873',
